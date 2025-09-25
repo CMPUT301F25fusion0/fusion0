645508372c460e364b752f4c7678630e144f7ebb
Bhoomi Sharma <bhoomisharma@Bhoomis-MacBook-Air-2.local>
2025-09-24 18:02:37 -0600

    Added color to shape

--- fusion0/app/src/main/Java/Shape.java
+++ fusion0/app/src/main/Java/Shape.java
@@ -1,4 +1,14 @@
 public abstract class Shape {
     int x;
     int y;
+
+    String color = "blue";
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
