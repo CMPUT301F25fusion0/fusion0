e7b9ce5b5ee0d59406d2badd961b5d5b42dbc5b0
Joshua Li <joshua12@ualberta.ca>
2025-09-24 18:35:35 -0600

    stuff

--- fusion0/app/src/main/Java/Shape.java
+++ fusion0/app/src/main/Java/Shape.java
@@ -1,9 +1,5 @@
 public abstract class Shape {
     int x;
     int y;
-<<<<<<< HEAD
-    String color = "blue";
-=======
     String color = "white";
->>>>>>> a593e9e4a299190b1e31f619e31cbb73b6dc3bb1
 }
